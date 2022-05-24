52e5f438b74ebac75f89c54c363c86e0a996f4de
xxzer <106092370+xxzerr@users.noreply.github.com>
2022-05-24 18:57:59 +0700

    Update Handler.jsx

--- pages/contact/src/Handler.jsx
+++ pages/contact/src/Handler.jsx
@@ -1,9 +1,9 @@
 function handleLink(id) {
   if(id == "ig") {
-    // window.location = "https://www.instagram.com/ryve.alns";
+    // window.location = "https://www.instagram.com/xx.zerrr";
     alert("instagram")
   }
-  else if(id == "email") alert("email");
+  else if(id == "discord") alert("discord");
   else if(id == "github") alert("github");
   else if(id == "profile") window.location = "../../../index.html"
   else PageNotFound();
